c6899a672f610d0deb9ea462b2d57eeaa4ce072b
thomasjunwon <junwon060521@gmail.com>
2025-01-28 14:20:38 +0900

    web css processing

--- cprapp/src/App.js
+++ cprapp/src/App.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-
-function Home() {
-  return <h2>home</h2>;
-}
-
-function About() {
-  return <h2>about</h2>;
-}
-
-function Services() {
-  return <h2>services</h2>;
-}
-
-function Contact() {
-  return <h2>contact</h2>;
-}
+import About from './pages/About';
+import CommunityBoard from './pages/CommunityBoard';
+import Home from './pages/Home';
+import MedicalBoard from './pages/MedicalBoard';
+import Schedules from './pages/Schedules';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
         <header>
-          <h1>심포니</h1>
-          <nav>
+          <h1 class='center-container'>심포니</h1>
+          <nav class='center-container'>
             <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
-              <li style={{ marginRight: '20px' }}><Link to="/">홈</Link></li>
-              <li style={{ marginRight: '20px' }}><Link to="/about">소개</Link></li>
-              <li style={{ marginRight: '20px' }}><Link to="/services">서비스</Link></li>
-              <li><Link to="/contact">연락처</Link></li>
+              <li style={{ marginRight: '40px',textDecoration:'none'}} class="head2"><Link to="/">홈</Link></li>
+              <li style={{ marginRight: '40px',textDecoration:'none' }} class="head2"><Link to="/about">소개</Link></li>
+              <li style={{ marginRight: '40px',textDecoration:'none' }} class="head2"><Link to="/medicalboard">의학게시판</Link></li>
+              <li style={{ marginRight: '40px',textDecoration:'none' }} class="head2"><Link to="/communityboard">활동게시판</Link></li>
+              <li class="head2"><Link to="/schedules">스케줄</Link></li>
             </ul>
           </nav>
         </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
+        <body>
+          <main>
+           <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/medicalboard" element={<MedicalBoard />} />
+              <Route path="/communityboard" element={<CommunityBoard />} />
+              <Route path="/schedules" element={<Schedules />} />
+            </Routes>
+          </main>
+        </body>
+        <footer>
+          <p>&copy;made by jun1</p>
+        </footer>
       </div>
     </Router>
   );
---
+++ cprapp/src/pages/CommunityBoard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CommunityBoard() {
+    return <h2>CRUD ACTIVITY</h2>;
+  }
+  
+export default CommunityBoard;
