1ee2b8decd2e92b19596e012e457ac6ca4ec8fd0
thomasjunwon <junwon060521@gmail.com>
2025-02-09 22:00:30 +0900

    cprback implemented

---
+++ cprback/src/test/java/cprbackend/cprback/CprbackApplicationTests.java
@@ -0,0 +1,13 @@
+package cprbackend.cprback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CprbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
